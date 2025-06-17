1207de04442053a7552b8defb3bc45a6cc10490f
Slonwer <ioserro53@gmail.com>
2025-06-16 21:02:36 -0300

    Container

--- terraform/Docker/main.tf
+++ terraform/Docker/main.tf
@@ -1,3 +1,18 @@
+provider "proxmox" {
+  pm_api_url      = "https://<SEU-PROXMOX-IP>:8006/api2/json"
+  pm_api_token_id = "<usuário>@pam!<nome-do-token>"
+  pm_api_token_secret = "<token-secreto>"
+  pm_tls_insecure = true  # Apenas para testes, desative em produção
+}
+
+variable "pve_node_name" {
+  default = "pve"
+}
+
+variable "storage_pool_name" {
+  default = "local-lvm"
+}
+
 resource "proxmox_vm_qemu" "web_1" {
   name        = "web_1"
   target_node = var.pve_node_name
